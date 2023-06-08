7b31f11c5f1a0f6835450d32860ba373e6e03cb8
Rishabh Naik <70832462+RishabhNaik@users.noreply.github.com>
2023-06-08 22:50:38 +0530

    Added new files

---
+++ portfolio/src/pages/about.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function About() {
+  return (
+    <div>
+        <h1>About</h1>
+        <p>lorem epsum lorem epsum lorem epsum lorem epsum lorem epsum lorem epsum lorem epsum lorem epsum lorem epsum lorem epsum lorem epsum lorem epsum lorem epsum lorem epsum lorem epsum lorem epsum lorem epsum </p>
+    </div>
+  )
+}
